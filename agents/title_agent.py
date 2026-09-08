417ad882d0d589d5245a847fa3ab7e66e7495b8a
1415908294-afk <1415908294@qq.com>
2026-09-08 23:58:29 +0800

    feat: add title agent

---
+++ agents/title_agent.py
@@ -0,0 +1,9 @@
+class TitleAgent:
+
+    def generate_titles(self, topic):
+
+        return [
+            f"3个方法解决{topic}",
+            f"90%的人不知道的{topic}技巧",
+            f"从0开始学会{topic}"
+        ]
